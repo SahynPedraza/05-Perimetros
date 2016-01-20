69a4f8807e166598e7cb422083870584abb169f8
SahynPedraza <dj.thesahyn19@gmail.com>
2016-01-20 15:38:09 -0600

    Perimetros
    Perimetros

---
+++ src/AppPerimetros.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author SAIN PEDRAZA GUERRERO
+ *
+ */
+public class AppPerimetros {
+/**
+ * 
+ * @param args metodo que llama a cuadrado para calcular
+ */
+	public static void main(String[] args) {
+		Perimetros p = new Perimetros();
+		System.out.printf("El perímetro del cuadrado es : %d",
+				p.cuadrado(10) );
+
+	}
+
+}
---
+++ src/Perimetros.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author SAIN PEDRAZA GUERRERO
+ *
+ */
+public class Perimetros {
+	/**
+	 * 
+	 * @param lado variable a sumar para obtener perimetro
+	 * @return
+	 */
+	public int cuadrado(int lado){
+		return lado+lado+lado+lado;
+	}
+}
